0658ef444a89b66b262c1de85529539d02e8b50f
SALIMARGOUB <salimargoub@gmail.com>
2023-03-31 11:36:35 +0200

    fonction animal vs element

---
+++ challenge6.php
@@ -0,0 +1,14 @@
+<?php
+  
+  function writeSecretSentence(string $animal,string $element): string 
+    {
+       
+        return $animal. ' s\'icline face'.' ' .$element;
+
+    }
+     $sentence=writeSecretSentence('tigre','feu');
+     echo $sentence;
+?>
+
+
+     
